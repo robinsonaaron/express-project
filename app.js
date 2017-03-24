fe35c9674c90d21cb209a89744aeb3f635d6c309
Aaron Robinson <aaronlrobinson@gmail.com>
2017-03-24 13:56:28 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('node_modules'));
+
+app.get('/', function(req, res) {
+    res.send('Hello world');
+
+});
+
+app.get('/books', function(req, res) {
+    res.send('Hello Books');
+
+});
+
+app.listen(port, function(err) {
+    console.log("running server on port: " + port);
+});
